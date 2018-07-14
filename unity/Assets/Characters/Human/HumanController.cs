47ed1305f0d946aba9229af873e2428ac9e6001a
Jonathan Cowling <09cowlingj@gmail.com>
2018-07-14 01:37:18 +0100

    Add Human Template
    includes:
    - Model
    - Rig
    - Animations (walk, idle, T-pose)
    
    references: #1

---
+++ unity/Assets/Characters/Human/HumanController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanController.cs : MonoBehaviour {
+
+	Animator animator
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// convert direction to polar
+		Vector2 direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+	}
+}
